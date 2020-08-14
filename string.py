5022fe85221ca60f93bedad90732ee7eada228eb
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-14 11:53:28 +0700

    Create string.py

---
+++ string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        q = (len(B) - 1) // len(A) + 1
+        for i in range(2):
+            if B in A * (q+i): return q+i
+        return -1
+val=Solution()
+a,b=map(str,input().split())
+print(val.repeatedStringMatch(a,b))
